40f2b8eddb13346194acc54c8bad7ad1853de164
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-19 16:44:10 +0900

    <fix>: <필요없는 코드 삭제>

--- src/components/Project3DImage.tsx
+++ src/components/Project3DImage.tsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import PHONE_MOCKUP from "../assets/image/react-shop-mockup.png";
 import PC_MOCKUP from "../assets/image/my-wedding-mockup.png";
 import PAD_MOCKUP from "../assets/image/team-mockup.png";
-import STUDY_BADGE_PAD from "../assets/image/study-badge-pad.png";
 
 const Project3DImage = ({ isHovered }: { isHovered: boolean }): JSX.Element => {
   return (
@@ -31,19 +30,6 @@ const Project3DImage = ({ isHovered }: { isHovered: boolean }): JSX.Element => {
         src={PAD_MOCKUP}
         className="w-[30%] md:w-[35%] absolute top-[-20%] left-[25%] md:top-[15%] z-20"
       />
-      {/* <motion.div
-        initial={{ opacity: 0, scale: 0 }}
-        animate={isHovered ? { opacity: 1, scale: 1.5, rotate: 0 } : {}}
-        style={{}}
-        transition={{ delay: 0.1 }}
-        className="absolute w-[80%] left-[20%] top-[10%] md:top-[130%] z-0"
-      >
-        <img src={PAD_MOCKUP} className="" />
-        <img
-          src={STUDY_BADGE_PAD}
-          className="absolute w-[21%] scale-y-125 rotate-[62deg] -skew-y-[38deg] top-[18%] left-[40%]"
-        />
-      </motion.div> */}
     </>
   );
 };
