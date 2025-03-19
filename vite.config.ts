495945696c15d5282d7938e03a501548bc9ec357
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-19 16:28:57 +0900

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // 자동 업데이트 활성화
+      devOptions: {
+        enabled: true, // 개발 환경에서도 PWA 동작 확인
+      },
+      manifest: {
+        name: "Geuna's portfolio",
+        short_name: "Geuna's portfolio",
+        description: "Hi, this is Geuna's portfolio",
+        theme_color: "#00000000",
+        background_color: "#ffffff",
+        display: "fullscreen",
+        icons: [
+          {
+            src: "/favicon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts-stylesheets",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1년
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
